0696578429d1a3a9d2f56bd3418e553c22fae4ca
guyattias <guyattias23@gmail.com>
2025-03-18 21:45:43 +0200

    init llm service

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { getConfig } from "./config/config";
+import { errorHandler } from "./middlewares";
+import { example } from "./router";
+
+dotenv.config();
+
+const app = express();
+const { port, env } = getConfig();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  next();
+});
+
+// Routes
+app.use("/example", example);
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`🚀 Server running on http://localhost:${port}`);
+});
